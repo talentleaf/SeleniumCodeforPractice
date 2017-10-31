e30d7f0e532cc9e66647fab63c790d5ec5507071
karpagam <karpagam.karuna@gmail.com>
2017-10-31 18:19:56 +0530

    Java Concepts

---
+++ JavaConcepts/src/HandlingString.java
@@ -0,0 +1,16 @@
+
+public class HandlingString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s="Welcome Selenium";
+		String e=String.valueOf(10);
+		s=s.toUpperCase();
+		s=s.toLowerCase();
+		System.out.println(s);
+		System.out.println(e);
+		//System.out.println(t);
+	}
+
+}
